0eb3bf227bdea56329219198d326dec84bc62eca
itc-23006 <n23006@std.it-college.ac.jp>
2024-01-24 06:27:28 +0000

    Chapter1.7
    headerとfooterの追加。

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 const Home = () => {
   return (
     <>
+      <header>HEADER</header>
+
       <main>
         <div>
           <h1>CUBE</h1>
           <p>アウトプットしていくサイト</p>
         </div>
       </main>
+
+      <footer>FOOTER</footer>
     </>
   )
 }
