e69470d3b1b23b70f0ae8968137dbfa4cf6184b1
itc-23006 <n23006@std.it-college.ac.jp>
2024-01-31 07:56:19 +0000

    Chapter7.4
    非同期処理

---
+++ pages/blog/schedule.js
@@ -0,0 +1,24 @@
+import { client } from 'lib/api'
+
+const Schedule = () => {
+  return <h1>記事のタイトル</h1>
+}
+
+const getStaticProps = async () => {
+  const resPromise = client.get({
+    endpoint: 'blogs'
+  })
+  resPromise.then(res => console.log(res)).catch(err => console.log(err))
+
+  console.log(await resPromise)
+  console.log('処理1')
+  setTimeout(() => console.log('処理2'), 1000)
+  console.log('処理3')
+
+  return {
+    props: {}
+  }
+}
+
+export default Schedule
+export { getStaticProps }
