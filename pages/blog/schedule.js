02f23110df42a64dd968f2f95fc0feaa3350448b
itc-23006 <n23006@std.it-college.ac.jp>
2024-02-06 06:15:37 +0000

    Chapter9.1
    microに変更。ローカルの代替画像を表示されるようになる。

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -13,6 +13,9 @@ import {
 } from 'components/two-column'
 import Image from 'next/image'
 
+// ローカルの代賛アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
 const Schedule = ({
   title,
   publish,
@@ -61,15 +64,17 @@ const Schedule = ({
 }
 
 const getStaticProps = async () => {
-  const slug = 'schedule'
+  const slug = 'micro'
   const post = await getPostBySlug(slug)
   const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description
     }
